3d09f878de9f00dafe0d37222a6ba39833e9eb82
Rasel Hossain <raselhossainy52@gmail.com>
2020-07-27 21:42:25 +0600

    Mizuxe added

---
+++ prectice_projects/E-commers/js/app.js
@@ -0,0 +1,29 @@
+const num1=100;
+const num2=50;
+let val;
+
+val =num1+num2;
+val =num1*num2;
+val =num1-num2;
+val =num1/num2;
+val =num1%num2;
+
+val=Math.PI;
+val=Math.E;
+val=Math.round(3.5);
+
+varl =99.;
+
+var rl=Math.round(varl);
+
+console.log(rl)
+
+val=Math.floor(5)
+val=Math.ceil(5.1)
+val=Math.abs(-5)
+val=Math.pow(5,2)
+val=Math.min(9,2,7,7,5,1)
+val=Math.max(5,7,8,4,0,259275290)
+val=Math.random();
+
+console.log(val);
